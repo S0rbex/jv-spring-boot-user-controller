11253f615fc7e6eea8c376d6a0332cb0557f37ea
S0rbex <vitalikmartunushun@gmail.com>
2026-02-08 22:32:02 +0200

    CHECKSTYLE

--- src/main/java/mate/academy/springboot/web/Application.java
+++ src/main/java/mate/academy/springboot/web/Application.java
@@ -2,8 +2,6 @@ package mate.academy.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.context.WebServerInitializedEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Application {
